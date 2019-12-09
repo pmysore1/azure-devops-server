30865c57c8209f8619899480724a00502ead5ffb
Mysore <pradeep.mysore@capgemini-gs.com>
2019-12-09 09:22:06 -0500

    initial

--- scripts/ConfigureTfsRemoteSql.ps1
+++ scripts/ConfigureTfsRemoteSql.ps1
@@ -11,6 +11,8 @@ $ErrorActionPreference = 'Stop'
 $TfsDownloadUrl = 'https://go.microsoft.com/fwlink/?LinkId=2089023'
 $InstallDirectory = 'C:\Program Files\Azure DevOps Server 2019'
 $InstallKey = 'HKLM:\SOFTWARE\Microsoft\DevDiv\tfs\Servicing\15.0\serverCore'
+$ssmsUrl = 'https://aka.ms/ssmsfullsetup'
+
 
 $PsToolsDownloadUrl = 'https://download.sysinternals.com/files/PSTools.zip'
 
@@ -59,6 +61,33 @@ function Ensure-TfsInstalled()
     {
         Write-Verbose "TFS is already installed"
     }
+}
+function installSQLManagementStudio()
+{
+    # Set file and folder path for SSMS installer .exe
+    $folderpath="c:\windows\temp"
+    $filepath="$folderpath\SSMS-Setup-ENU.exe"
+    
+    #If SSMS not present, download
+    if (!(Test-Path $filepath)) {
+        write-host "Downloading SQL Server 2016 SSMS..."
+        $URL = "https://download.microsoft.com/download/3/1/D/31D734E0-BFE8-4C33-A9DE-2392808ADEE6/SSMS-Setup-ENU.exe"
+        $clnt = New-Object System.Net.WebClient
+        $clnt.DownloadFile($url,$filepath)
+        Write-Host "SSMS installer download complete" -ForegroundColor Green
+        
+    }
+    else {
+    
+        write-host "Located the SQL SSMS Installer binaries, moving on to install..."
+    }
+    
+    # start the SSMS installer
+    write-host "Beginning SSMS 2016 install..." -nonewline
+    $Parms = " /Install /Quiet /Norestart /Logs log.txt"
+    $Prms = $Parms.Split(" ")
+    & "$filepath" $Prms | Out-Null
+    Write-Host "SSMS installation complete" -ForegroundColor Green
 }
 
 function Download-PsTools()
